0c6c35229441c7e9f95bde95907a1e75681944c3
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-02-10 14:55:34 -0800

    fix logger middleware

--- src/sciop/main.py
+++ src/sciop/main.py
@@ -6,7 +6,6 @@ from fastapi import FastAPI
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi_pagination import add_pagination
-from py_fastapi_logging.middlewares.logging import LoggingMiddleware
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.middleware import SlowAPIMiddleware
 from starlette.middleware.cors import CORSMiddleware
@@ -16,7 +15,7 @@ from sciop.config import config
 from sciop.const import STATIC_DIR
 from sciop.db import create_tables
 from sciop.frontend.main import frontend_router
-from sciop.logging import init_logger
+from sciop.logging import LoggingMiddleware, init_logger
 from sciop.middleware import limiter
 
 # def custom_generate_unique_id(route: APIRoute) -> str:
@@ -41,12 +40,7 @@ app = FastAPI(
 
 app.state.limiter = limiter
 app.add_middleware(SlowAPIMiddleware)
-app.add_middleware(
-    LoggingMiddleware,
-    app_name="sciop",
-    logger=init_logger("sciop.requests"),
-    exclude_requests_starting_with=["/static", "/torrent"],
-)
+app.add_middleware(LoggingMiddleware, logger=init_logger("requests"))
 app.add_exception_handler(429, _rate_limit_exceeded_handler)
 
 # Set all CORS enabled origins
@@ -74,4 +68,6 @@ app.add_middleware(GZipMiddleware, minimum_size=500, compresslevel=5)
 
 
 def main() -> None:
-    uvicorn.run("sciop.main:app", host=config.host, port=config.port, reload=config.reload)
+    uvicorn.run(
+        "sciop.main:app", host=config.host, port=config.port, reload=config.reload, lifespan="on"
+    )
