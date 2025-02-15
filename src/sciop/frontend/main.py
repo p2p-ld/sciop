761198cf144608e27f5838091f70664a39880917
William Waites <w.waites@soton.ac.uk>
2025-02-15 01:44:34 +0000

    seriously? ruff again

--- src/sciop/frontend/main.py
+++ src/sciop/frontend/main.py
@@ -4,7 +4,7 @@ from sciop.api.deps import add_htmx_response_trigger
 from sciop.frontend.accounts import accounts_router
 from sciop.frontend.datasets import datasets_router
 from sciop.frontend.index import index_router
-from sciop.frontend.rdf import rdf_router, id_router
+from sciop.frontend.rdf import id_router, rdf_router
 from sciop.frontend.rss import rss_router
 from sciop.frontend.self import self_router
 from sciop.frontend.uploads import uploads_router
