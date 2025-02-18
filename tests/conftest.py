37870bd118e9df42b17681d13ea42bf97ff0c4f5
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-02-17 20:44:54 -0800

    headless via attr

--- tests/conftest.py
+++ tests/conftest.py
@@ -162,6 +162,7 @@ async def driver(run_server: Server_) -> webdriver.Firefox:
     options = FirefoxOptions()
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--headless")
+    options.headless = True
     options.add_argument(f"--window-size={1920},{1080}")
     _service = FirefoxService(executable_path=manager)
     browser = webdriver.Firefox(service=_service, options=options)
