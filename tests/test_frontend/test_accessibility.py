3e8521b0786ccea99457e3d03f03393832fecf05
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-02-20 00:29:35 -0800

    fix headers and tests

--- tests/test_frontend/test_accessibility.py
+++ tests/test_frontend/test_accessibility.py
@@ -1,16 +1,40 @@
 import pytest
 from bs4 import BeautifulSoup
+import re
 
-TEST_PAGES = ("/", "/datasets", "/datasets/default", "/uploads/defaultt")
+TEST_PAGES = (
+    "/",
+    "/datasets",
+    "/datasets/default",
+    "/uploads/defaultt",
+    "/login",
+    "/self",
+    "/self/review",
+    "/self/admin",
+    "/self/log",
+)
 
 
 @pytest.mark.parametrize("page", TEST_PAGES)
-def test_has_h1(client, page, default_db):
+def test_headers(client, page, default_db, admin_auth_header):
     """
-    Pages should all have an h1 at the top of the page
+    Pages should
+    - all have an h1 at the top of the page
+    - have no (positive) jumps in header levels
+
+    TODO: These should probably be selenium tests but i am so tired
     """
-    result = client.get(page)
+    result = client.get(page, headers=admin_auth_header)
+    assert result.status_code == 200
     soup = BeautifulSoup(result.content, "lxml")
-    container = soup.find("div", {"class": "container"})
-    flat_nodes = container.find_all(True)
-    assert [elt.name == "h1" for elt in flat_nodes[0:5]]
+
+    # First header is h1
+    all_headers = soup.find_all(re.compile("^h[1-6]$"))
+    assert all_headers[0].name == "h1"
+
+    # headers don't have jumps
+    if len(all_headers) > 1:
+        header_levels = [int(head.name[-1]) for head in all_headers]
+        diff = [header_levels[i] - header_levels[i - 1] for i in range(1, len(header_levels))]
+        one_or_below = [d <= 1 for d in diff]
+        assert all(one_or_below)
