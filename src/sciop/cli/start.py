a43a4cf68b0d66f2cdc521841d9e7ce4910f612b
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-03-23 00:57:40 -0700

    cli

---
+++ src/sciop/cli/start.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+import click
+
+
+@click.command()
+@click.option("--port", required=False, type=int)
+def start(port: Optional[int] = None) -> None:
+    """Start the sciop development server"""
+    from sciop.config import config
+    from sciop.main import main
+
+    if port is not None:
+        config.port = port
+
+    main(config=config)
