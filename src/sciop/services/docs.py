86f9dd7152926f2111d0783cf9d81ada3f2ad1ad
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-04-07 20:50:05 -0700

    only build docs once on startup

--- src/sciop/services/docs.py
+++ src/sciop/services/docs.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from time import time
 from urllib.parse import urljoin
 
 from mkdocs import config as mkdocs_config
@@ -20,21 +21,21 @@ def build_docs() -> None:
     Mimicking the cli action:
     https://github.com/mkdocs/mkdocs/blob/7e4892ab2dd4a52efd95a9de407eee63310e0780/mkdocs/__main__.py#L280
     """
+    logger = init_logger("services.docs")
     config_file = _find_mkdocs_config()
     if not config_file.exists():
-        logger = init_logger("services.docs")
         logger.warning("Docs could not be built, no mkdocs config was found at %s", config_file)
         return
 
-    cfg = mkdocs_config.load_config(config_file=str(config_file))
-
-    dirty = False
-
-    cfg.plugins.on_startup(command="build", dirty=dirty)
-
     output_dir = Path(__file__).parents[1] / "docs"
     output_dir.mkdir(exist_ok=True)
+    index = output_dir / "index.html"
+    if index.exists() and (time() - index.stat().st_mtime) < 10:
+        logger.debug("Not rebuilding docs, built less than 10 seconds ago")
+        return
+
+    cfg = mkdocs_config.load_config(config_file=str(config_file))
+    cfg.plugins.on_startup(command="build", dirty=False)
     cfg.site_dir = output_dir
     cfg.site_url = urljoin(config.external_url, "/docs")
-
-    build.build(cfg, dirty=dirty)
+    build.build(cfg, dirty=False)
