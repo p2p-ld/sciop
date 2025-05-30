a09aecb2919acd8e881d3f861ff7f04a442b56b9
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-05-29 22:18:47 -0700

    add DatasetClaim to docs, add some didactic docstrings

--- src/sciop/models/claim.py
+++ src/sciop/models/claim.py
@@ -1,7 +1,14 @@
+"""
+Models for coordinating automated dispersed scraping using dataset parts and
+[`sciop-scraping`](https://codeberg.org/Safeguarding/sciop-scraping)
+
+Powering the sciopteam swarrior, aka: The Kronkelianator 2.
+"""
+
 from enum import StrEnum
 from typing import Optional, Union
 
-from pydantic import field_validator, model_validator
+from pydantic import model_validator
 from sqlalchemy.schema import UniqueConstraint
 from sqlmodel import Field, Relationship
 
@@ -13,15 +20,39 @@ from sciop.types import IDField, SlugStr, UsernameStr, UTCDateTime
 
 
 class ClaimStatus(StrEnum):
+    """
+    An initial claim is made with in_progress,
+    then marked as completed after the scrape is finished.
+    When the upload is made the claim is removed.
+    """
+
     in_progress = "in_progress"
+    """The account has indicated that it is currently scraping the item"""
     completed = "completed"
+    """The account has finished scraping the item, but has not yet uploaded a torrent for it"""
 
 
 class DatasetClaimBase(SQLModel):
+    """Fields shared by all models in the DatasetClaim family"""
+
     status: ClaimStatus = Field(description="The current status of the dataset")
 
 
 class DatasetClaim(DatasetClaimBase, TableMixin, table=True):
+    """
+    ORM model for DatasetClaim
+
+    A DatasetClaim is always associated with a [`Dataset`][sciop.models.Dataset],
+    and optionally with a [`DatasetPart`][sciop.models.DatasetPart].
+    (The presence of a Dataset is thus not distinguishing between
+    a claim on a Dataset and one of its parts,
+    and one needs to check if `dataset_part` is `None`)
+
+    This ends up saving us a tiny bit of time not needing to join to find the dataset.
+
+    Each [account][sciop.models.Account] can only have one claim per dataset or dataset part.
+    """
+
     __tablename__ = "dataset_claim"
     __table_args__ = (
         UniqueConstraint(
@@ -44,6 +75,14 @@ class DatasetClaim(DatasetClaimBase, TableMixin, table=True):
 
 
 class DatasetClaimRead(DatasetClaimBase, TableMixin):
+    """
+    Version of DatasetClaim used when returning from the API or displaying publicly.
+
+    `*Read` models are used to transform values from their database representation to an
+    externally useful one (e.g. substituting the entire [`Dataset`][sciop.models.Dataset]
+    model for its slug), as well as remove any internal fields, etc.
+    """
+
     created_at: UTCDateTime
     updated_at: UTCDateTime
     dataset: SlugStr
@@ -53,7 +92,13 @@ class DatasetClaimRead(DatasetClaimBase, TableMixin):
     @model_validator(mode="before")
     @classmethod
     def orm_to_strings(cls, data: Union[dict, "DatasetClaim"]) -> dict:
-        """Get slugs from dataset or dataset part"""
+        """
+        Convert ORM models to their string shorthands
+
+        - `dataset` to [slug][sciop.models.Dataset.slug]
+        - `dataset_part` to its [part_slug][sciop.models.DatasetPart.part_slug]
+        - `account` to [username][sciop.models.Account.username]
+        """
         if isinstance(data, DatasetClaim):
             dumped = data.model_dump(exclude_none=True)
             dumped["dataset"] = data.dataset.slug
@@ -61,11 +106,3 @@ class DatasetClaimRead(DatasetClaimBase, TableMixin):
             dumped["account"] = data.account.username
             return dumped
         return data
-
-    @field_validator("account", mode="before")
-    @classmethod
-    def account_to_username(cls, value: Account | str) -> SlugStr:
-        """Replace account object with username"""
-        if not isinstance(value, str):
-            value = value.username
-        return value
