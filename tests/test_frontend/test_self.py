5f3145f697f226ecb37d2f6424f563ce50a58a13
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-09-02 12:29:50 -0700

    fix account admin page

--- tests/test_frontend/test_self.py
+++ tests/test_frontend/test_self.py
@@ -1,4 +1,26 @@
 import pytest
+from playwright.async_api import Page, expect
+
+from sciop.models import Account
+
+
+@pytest.mark.playwright
+@pytest.mark.asyncio(loop_scope="session")
+async def test_account_admin(page_as_admin: Page, account, session):
+    page = page_as_admin
+    rando: Account = account(username="rando")
+    assert not rando.has_scope("upload")
+
+    await page.goto("http://127.0.0.1:8080/self/admin")
+    await expect(page.locator("#rando-scope-upload")).to_have_class("toggle-button")
+    await page.locator("#rando-scope-upload").click()
+    await expect(page.locator("#rando-scope-upload")).to_have_class("toggle-button checked")
+
+    session.refresh(rando)
+    assert rando.has_scope("upload")
+
+    await page.locator("#rando-scope-upload").click()
+    await expect(page.locator("#rando-scope-upload")).to_have_class("toggle-button")
 
 
 @pytest.mark.skip()
