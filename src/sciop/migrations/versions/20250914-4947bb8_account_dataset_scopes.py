8dd07f755408374c60b9c6cce5312c979dd287c3
lina <codeberg@spectraldiscord.com>
2025-09-15 17:11:58 -0400

    migration head fix

--- src/sciop/migrations/versions/20250914-4947bb8_account_dataset_scopes.py
+++ src/sciop/migrations/versions/20250914-4947bb8_account_dataset_scopes.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "4947bb87abc6"
-down_revision: Union[str, None] = "efb45cd6cac9"
+down_revision: Union[str, None] = "99812ae0d9b1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
