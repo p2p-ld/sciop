989cca5401d0e074dbd2b5cc7d1659005b8c6997
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-03-16 13:53:39 -0700

    remove unused keep_defaults param, default type of defualt_factory is None

--- tests/test_api/test_docs.py
+++ tests/test_api/test_docs.py
@@ -1,4 +1,4 @@
 def test_api_docs_render(client):
     """The API docs should load and not fail to load!!!"""
-    response = client.get("/api/docs")
+    response = client.get("/docs/api")
     assert response.status_code == 200
